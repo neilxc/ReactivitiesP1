465a68b2cffeb75ba567d77b7dc6f5726dd79c15
trycatchlearn <trycatchlearn@outlook.com>
2019-04-01 14:44:39 +0100

    Added Client App

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => opt.AddPolicy("CorsPolicy", 
+                builder => 
+                {
+                    builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+                }
+            ));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -46,8 +52,8 @@ namespace API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+            app.UseHttpsRedirection();  
             app.UseMvc();
         }
     }
